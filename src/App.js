e67fe08f48ac443c509c5e90f2fcad06e38162c8
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-08-11 16:20:57 +0100

    added fonts, added header and styled header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Featured from "./Components/Featured.jsx";
+import Home from "./Components/Home.jsx";
+import Navbar from "./Components/Navbar.jsx";
+import Projects from "./Components/Projects.jsx";
+import './Styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar />
       </header>
+      <main>
+        <Home />
+        <Featured />
+        <Projects />
+      </main>
     </div>
   );
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import '../Styles/Navbar.css';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import {FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Navbar = () => {
+    const [menu, openMenu] = useState(false);
+
+    const handleMenu = (e) => {
+        if (e.target){
+            openMenu(!menu);
+        };
+    };
+
+    return (
+        <div className="header">
+            <div className="name">
+                <a href="/">
+                    <span>&lt;</span>
+                    Abdifatah Basher
+                    <span>/&gt;</span>
+                </a>
+            </div>
+            <div className="navbar">
+                <ul className={ menu ? "menu-open" : ""}>
+                    <li><a href="/#home">Home</a></li>
+                    <li><a href="/#works">Works</a></li>
+                    <li><a href="/#about">About</a></li>
+                    <li><a href="/#contact">Contact</a></li>
+                </ul>
+            </div>
+            <div className="icons">
+                <a href="/github"><FontAwesomeIcon icon={faGithub} size="2x"/></a>
+                <a href="/linkedin"><FontAwesomeIcon icon={faLinkedin} size="2x"/></a>
+            </div>
+            <div className="burger" onClick={handleMenu}>
+                <div className={menu ? "line1" : ""}></div>
+                <div className={menu ? "line2" : ""}></div>
+                <div className={menu ? "line3" : ""}></div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
