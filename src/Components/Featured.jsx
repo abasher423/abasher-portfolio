6e7a3218397ed061a4c1679faf93635badf56128
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-08-29 15:41:10 +0100

    updated text, added responsiveness for smartphones

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -6,15 +6,21 @@ import mui from '../images/mui.png';
 import mongo from '../images/mongo.png';
 import htmlcss from '../images/html-css.png';
 import  {FontAwesomeIcon}  from '@fortawesome/react-fontawesome';
-import { faBookmark } from '@fortawesome/free-regular-svg-icons'
-import { faHandshake } from '@fortawesome/free-regular-svg-icons'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
+import { faHandshake } from '@fortawesome/free-solid-svg-icons'
 import { faGraduationCap } from '@fortawesome/free-solid-svg-icons'
+import Aos from 'aos';
+import { useEffect } from 'react';
 
 const Featured = () => {
+    useEffect(() => {
+        Aos.init({duration: 2000});
+    }, []);
     return (
-        <div className="container">
+        <div id="home">
+            <div className="featured-container">
             <div className="featured">
-                <div className="featured-text">
+                <div className="featured-text" data-aos="fade-left">
                     <h4>Featured Project</h4>
                     <br />
                     <h2>Velular E-Commerce</h2>
@@ -32,40 +38,41 @@ const Featured = () => {
                         <img src={htmlcss} alt="html and css" />
                     </div>
                 </div>
-                <div className="featured-image">
+                <div className="featured-image" data-aos="fade-right">
                     <a href="https://velular.herokuapp.com">
                         <img src={image} alt="velular" />
                     </a>
                 </div>
             </div>
+            </div>
             <div className="cards-container">
-                <div className="card1">
-                    <div className="box">
-                        <div className="card-icon">
-                            <FontAwesomeIcon icon={faBookmark} />
-                        </div>
-                        <h2 className="card-title">
-                            Adaptable
-                        </h2>
-                        <p className="card-text">
-                            Able to grasp new concepts quickly with an excellent capicity to retain new information
-                        </p>
-                    </div>
-                </div>
-                <div className="card2">
+                <div className="card1" data-aos="fade-right" data-aos-delay="400">
                     <div className="box">
                         <div className="card-icon">
                             <FontAwesomeIcon icon={faGraduationCap} />
                         </div>
                         <h2 className="card-title">
-                            Bachelors
+                            Bachelors     
                         </h2>
                         <p className="card-text">
-                            Computer Science graduate achieving <b>first</b> class with honors
+                            Bachelor of Science with Honours in Computer Science, Class I.  
                         </p>
                     </div>
                 </div>
-                <div className="card3">
+                <div className="card2" data-aos="fade-right" data-aos-delay="600">
+                    <div className="box">
+                        <div className="card-icon">
+                            <FontAwesomeIcon icon={faBookmark} />   
+                        </div>
+                        <h2 className="card-title">
+                            Adaptable
+                        </h2>
+                        <p className="card-text">
+                            Able to grasp new concepts quickly with an excellent capicity to retain new information.
+                        </p>
+                    </div>
+                </div>
+                <div className="card3" data-aos="fade-right" data-aos-delay="1000">
                     <div className="box">
                         <div className="card-icon">
                             <FontAwesomeIcon icon={faHandshake} />
@@ -74,7 +81,7 @@ const Featured = () => {
                                 Reliable
                         </h1>
                         <p className="card-text">
-                            Highly motivated individual looking to kickstart his career as a Web Developer
+                            Highly motivated individual looking to kickstart his career as a Web Developer.
                         </p>
                     </div>
                 </div>
