f2191fc69321d4516c76d71367253035171756e3
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-08-29 15:42:53 +0100

    updated name for navbar, added responsiveness for iphone

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import '../Styles/Navbar.css';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { Icon } from '@iconify/react';
 import {FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Navbar = () => {
@@ -17,22 +18,22 @@ const Navbar = () => {
         <div className="header">
             <div className="name">
                 <a href="/">
-                    <span>&lt;</span>
-                    A.B
-                    <span>/&gt;</span>
+                    {/* <span>&lt;</span> */}
+                    <Icon icon="teenyicons:ab-testing-outline" data-flip="vertical"/>
+                    {/* <span>/&gt;</span> */}
                 </a>
             </div>
             <div className="navbar">
                 <ul className={ menu ? "menu-open" : ""}>
-                    <li><a href="/#home">Home</a></li>
-                    <li><a href="/#works">Works</a></li>
-                    <li><a href="/#about">About</a></li>
-                    <li><a href="/#contact">Contact</a></li>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
                 </ul>
             </div>
             <div className="icons">
-                <a href="/github"><FontAwesomeIcon icon={faGithub} size="2x"/></a>
-                <a href="/linkedin"><FontAwesomeIcon icon={faLinkedin} size="2x"/></a>
+                <a href="https://github.com/abasher423"><FontAwesomeIcon icon={faGithub} size="2x"/></a>
+                <a href="https://www.linkedin.com/in/abdifatah-basher-074684194/"><FontAwesomeIcon icon={faLinkedin} size="2x"/></a>
             </div>
             <div className="burger" onClick={handleMenu}>
                 <div className={menu ? "line1" : ""}></div>
