e7e45615a4b1a4a0532f76c023d56cdc92c83ae3
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-08-29 15:41:56 +0100

    updated bakground, initial styles and added new sections

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import About from "./Components/About.jsx";
+import Contact from "./Components/Contact.jsx";
 import Featured from "./Components/Featured.jsx";
+import Footer from "./Components/Footer.jsx";
 import Home from "./Components/Home.jsx";
 import Navbar from "./Components/Navbar.jsx";
 import Projects from "./Components/Projects.jsx";
+import Skills from "./Components/Skills.jsx";
 import './Styles/App.css';
 
 function App() {
@@ -14,7 +18,13 @@ function App() {
         <Home />
         <Featured />
         <Projects />
+        <About />
+        <Skills />
+        <Contact />
       </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
