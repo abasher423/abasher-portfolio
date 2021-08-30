492085a5f9b2a001bb629d598bbe8795e5f67cf3
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-08-30 03:16:14 +0100

    created skills section, added styling for cards

---
+++ src/Components/Skills.jsx
@@ -0,0 +1,64 @@
+import '../Styles/Skills.css';
+import { Icon } from '@iconify/react';
+import Aos from 'aos';
+import { useEffect } from 'react';
+
+const Skills = () => {
+    useEffect(() => {
+        Aos.init({duration: 2000});
+    }, [])
+    return (
+        <div className="skills-container">
+            <h2>Skills</h2>
+            <div className="skills">
+                <div className="card" data-aos="zoom-in">
+                    <div>
+                        <Icon icon="bx:bx-code-alt" />
+                    </div>
+                    <h3>Languages</h3>
+                    <p>HTML, CSS, Sass, JavaScript, Node</p>
+                </div>
+                <div className="card" data-aos="zoom-in">
+                    <div>
+                        <Icon icon="akar-icons:react-fill" />
+                    </div>
+                    <h3>Frameworks</h3>
+                    <p>React, Express, Vue</p>
+                </div>
+                <div className="card" data-aos="zoom-in">
+                    <div>
+                        <Icon icon="mdi:material-ui" />
+                    </div>
+                    <h3>Libraries</h3>
+                    <p>Material-UI, Vuetify, Redux</p>
+                </div>
+                <div className="card" data-aos="zoom-in">
+                    <div>
+                        <Icon icon="akar-icons:figma-fill" />
+                    </div>
+                    <h3>Tools</h3>
+                    <p>Figma, Postman, Visual Studio Code</p>
+                </div>
+                <div className="card" data-aos="zoom-in">
+                    <div>
+                        <Icon icon="dashicons:database" />
+                    </div>
+                    <h3>Databases</h3>
+                    <p>SQLite, MySQL, MongoDB</p>
+                </div>
+                <div className="card" data-aos="zoom-in">
+                    <div>
+                        <Icon icon="bx:bx-git-branch" />
+                    </div>
+                    <h3>Version Control</h3>
+                    <p>Git, GitHub</p>
+                </div>
+            </div>
+            <div className="interests">
+
+            </div>
+        </div>
+    );
+};
+
+export default Skills;
